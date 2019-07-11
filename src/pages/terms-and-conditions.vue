a489171d72bb1cc903f86a5225e1768399698fab
Blake Newman <blakenoodles@gmail.com>
2019-07-11 11:32:11 +0100

    Fix legal page styling

--- src/pages/terms-and-conditions.vue
+++ src/pages/terms-and-conditions.vue
@@ -1,109 +1,109 @@
 <template>
-  <div class="container">
-    <div class="container__inner">
-      <heading>Terms and conditions</heading>
+  <main>
+    <page-section>
+      <heading variant="block">Terms and conditions</heading>
       <heading level="3">Last updated: June 08, 2019</heading>
-      <p>
+      <paragraph>
         Please read these Terms and Conditions ("Terms", "Terms and Conditions") carefully before
         using the https://vuejs.london website (the "Service") operated by Vue.js Limited ("us",
         "we", or "our").
-      </p>
-      <p>
+      </paragraph>
+      <paragraph>
         Your access to and use of the Service is conditioned on your acceptance of and compliance
         with these Terms. These Terms apply to all visitors, users and others who access or use the
         Service.
-      </p>
-      <p>
+      </paragraph>
+      <paragraph>
         By accessing or using the Service you agree to be bound by these Terms. If you disagree with
         any part of the terms then you may not access the Service.
-      </p>
+      </paragraph>
       <heading level="2">Purchases</heading>
-      <p>
+      <paragraph>
         You are encouraged to familiarise yourself with your rights contained within the Sale of
         Goods Act 1979, Unfair Contract Terms Act 1977 and the Unfair Terms in Consumer Contracts
         Regulations 1999.
-      </p>
-      <p>
+      </paragraph>
+      <paragraph>
         If you wish to purchase any product or service made available through the Service
         ("Purchase"), you may be asked to supply certain information relevant to your Purchase
         including, without limitation, your credit card number, the expiration date of your credit
         card, your billing address, and your shipping information.
-      </p>
-      <p>
+      </paragraph>
+      <paragraph>
         You represent and warrant that: (i) you have the legal right to use any credit card(s) or
         other payment method(s) in connection with any Purchase; and that (ii) the information you
         supply to us is true, correct and complete. You expressly agree that Vue.js Limited is not
         responsible for any loss or damage arising from the submission of false or inaccurate
         information.
-      </p>
-      <p>
+      </paragraph>
+      <paragraph>
         By submitting such information, you grant us the right to provide the information to third
         parties for purposes of facilitating the completion of Purchases.
-      </p>
-      <p>
+      </paragraph>
+      <paragraph>
         We reserve the right to refuse or cancel your order at any time for certain reasons
         including but not limited to: product or service availability, errors in the description or
         price of the product or service, error in your order or other reasons. You expressly agree
         that Vue.js Limited cannot accept any liability for loss or damage arising out of such
         cancellation.
-      </p>
-      <p>
+      </paragraph>
+      <paragraph>
         We reserve the right to refuse or cancel your order if fraud or an unauthorised or illegal
         transaction is suspected.
-      </p>
+      </paragraph>
       <heading level="2">Availability, Errors and Inaccuracies</heading>
-      <p>
+      <paragraph>
         We are constantly updating our offerings of products and services on the Service. The
         products or services available on our Service may be mispriced, described inaccurately, or
         unavailable, and we may experience delays in updating information on the Service and in our
         advertising on other web sites. You expressly agree that any such offer of a product or
         service does not constitute a legal offer capable of attracting legal consequences.
-      </p>
-      <p>
+      </paragraph>
+      <paragraph>
         We cannot and do not guarantee the accuracy or completeness of any information, including
         prices, product images, specifications, availability, and services. We reserve the right to
         change or update information and to correct errors, inaccuracies, or omissions at any time
         without prior notice. Section "Availability, Errors and Inaccuracies" is without prejudice
         to existing statutory rights.
-      </p>
+      </paragraph>
       <heading level="2">Links To Other Web Sites</heading>
-      <p>
+      <paragraph>
         Our Service may contain links to third-party web sites or services that are not owned or
         controlled by Vue.js Limited.
-      </p>
-      <p>
+      </paragraph>
+      <paragraph>
         Vue.js Limited has no control over, and assumes no responsibility for, the content, privacy
         policies, or practices of any third party web sites or services. You further acknowledge and
         agree that Vue.js Limited shall not be responsible or liable, directly or indirectly, for
         any damage or loss caused or alleged to be caused by or in connection with use of or
         reliance on any such content, goods or services available on or through any such web sites
         or services.
-      </p>
-      <p>
+      </paragraph>
+      <paragraph>
         We strongly advise you to read the terms and conditions and privacy policies of any
         third-party web sites or services that you visit.
-      </p>
+      </paragraph>
       <heading level="2">Termination</heading>
-      <p>
+      <paragraph>
         We may terminate or suspend your access immediately, without prior notice or liability, for
         any reason whatsoever, including without limitation if you breach the Terms.
-      </p>
-      <p>Upon termination, your right to use the Service will immediately cease.</p>
-      <p>
+      </paragraph>
+      <paragraph>Upon termination, your right to use the Service will immediately cease.</paragraph>
+      <paragraph>
         All provisions of the Terms which by their nature should survive termination shall survive
         termination, including, without limitation, ownership provisions, warranty disclaimers,
         indemnity and limitations of liability.
-      </p>
+      </paragraph>
       <heading level="2">Indemnification</heading>
-      <p>
+      <paragraph>
         You agree to defend, indemnify and hold harmless Vue.js Limited and its licensee and
         licensors, and their employees, contractors, agents, officers and directors, from and
         against any and all claims, damages, obligations, losses, liabilities, costs or debt, and
         expenses (including but not limited to attorney's fees), resulting from or arising out of a)
         your use and access of the Service, or b) a breach of these Terms.
-      </p>
+      </paragraph>
       <heading level="2">Limitation Of Liability</heading>
-      <p>
+      <paragraph>
         In no event shall Vue.js Limited, nor its directors, employees, partners, agents, suppliers,
         or affiliates, be liable for any indirect, incidental, special, consequential or punitive
         damages, including without limitation, loss of profits, data, use, goodwill, or other
@@ -114,22 +114,22 @@
         or any other legal theory, whether or not we have been informed of the possibility of such
         damage, and even if a remedy set forth herein is found to have failed of its essential
         purpose.
-      </p>
+      </paragraph>
       <heading level="2">Disclaimer</heading>
-      <p>
+      <paragraph>
         Your use of the Service is at your sole risk. The Service is provided on an "AS IS" and "AS
         AVAILABLE" basis. The Service is provided without warranties of any kind, whether express or
         implied, including, but not limited to, implied warranties of merchantability, fitness for a
         particular purpose, non-infringement or course of performance.
-      </p>
-      <p>
+      </paragraph>
+      <paragraph>
         Vue.js Limited its subsidiaries, affiliates, and its licensors do not warrant that a) the
         Service will function uninterrupted, secure or available at any particular time or location;
         b) any errors or defects will be corrected; c) the Service is free of viruses or other
         harmful components; or d) the results of using the Service will meet your requirements.
-      </p>
+      </paragraph>
       <heading level="2">Exclusions</heading>
-      <p>
+      <paragraph>
         Without limiting the generality of the foregoing and notwithstanding any other provision of
         these terms, under no circumstances will Vue.js Limited ever be liable to you or any other
         person for any indirect, incidental, consequential, special, punitive or exemplary loss or
@@ -142,45 +142,48 @@
         (including without limitation gross negligence and fundamental breach) by Vue.js Limited or
         any person for whom Vue.js Limited is responsible, and even if Vue.js Limited has been
         advised of the possibility of such loss or damage being incurred.
-      </p>
+      </paragraph>
       <heading level="2">Governing Law</heading>
-      <p>
+      <paragraph>
         These Terms shall be governed and construed in accordance with the laws of England and
         Wales, without regard to its conflict of law provisions.
-      </p>
-      <p>
+      </paragraph>
+      <paragraph>
         Our failure to enforce any right or provision of these Terms will not be considered a waiver
         of those rights. If any provision of these Terms is held to be invalid or unenforceable by a
         court, the remaining provisions of these Terms will remain in effect. These Terms constitute
         the entire agreement between us regarding our Service, and supersede and replace any prior
         agreements we might have between us regarding the Service.
-      </p>
+      </paragraph>
       <heading level="2">Changes</heading>
-      <p>
+      <paragraph>
         We reserve the right, at our sole discretion, to modify or replace these Terms at any time.
         If a revision is material we will try to provide at least 30 days notice prior to any new
         terms taking effect. What constitutes a material change will be determined at our sole
         discretion.
-      </p>
-      <p>
+      </paragraph>
+      <paragraph>
         By continuing to access or use our Service after those revisions become effective, you agree
         to be bound by the revised terms. If you do not agree to the new terms, you must stop using
         the service.
-      </p>
+      </paragraph>
       <heading level="2">Privacy Policy and Cookie Policy</heading>
-      <p>
+      <paragraph>
         Please refer to our Privacy Policy and Cookies Policy. You agree that they constitute part
         of these terms. You must read our Privacy Policy and Cookies Policy before you use the
         Service.
-      </p>
+      </paragraph>
       <heading level="2">Contact Us</heading>
-      <p>If you have any questions about these Terms, please contact us.</p>
-    </div>
-  </div>
+      <paragraph>If you have any questions about these Terms, please contact us.</paragraph>
+    </page-section>
+  </main>
 </template>
 
 <script>
 import Heading from '~/components/Heading.vue'
+import PageSection from '~/components/PageSection'
+import Paragraph from '~/components/Paragraph'
+
 export default {
   head: {
     title: 'Vue.js London 2019 | Terms and Conditions',
@@ -188,6 +191,8 @@ export default {
 
   components: {
     Heading,
+    PageSection,
+    Paragraph,
   },
 }
 </script>
